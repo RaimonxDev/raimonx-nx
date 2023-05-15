d73a19f211e05aa53c4cb1071fd880b9f1d0ab96
Ramon <ramonmartinezcordero@gmail.com>
2023-05-15 08:22:48 -0400

    initial

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
+
+interface Theme {
+  [key: string]: {
+    [key: string]: {
+      [key: string]: string;
+    };
+  };
+}
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, CommonModule, MatButtonModule],
   selector: 'raimonx-dev-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio';
+
+  userTheme: any = {}
+
+
+  theme: Theme = {
+    default_theme: {
+      primary: {
+        50: '#eef2ff',
+        100: '#e0e7ff',
+        200: '#c7d2fe',
+        DEFAULT: '#a5b4fc',
+      },
+      accent: {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        DEFAULT: '#cbd5e1',
+      },
+    },
+    alter: {
+      primary: {
+        50: '#fef2f2',
+        100: '#fee2e2',
+        200: '#fecaca',
+        DEFAULT: '#fca5a5',
+      },
+      accent: {
+        50: '#74b0f5',
+        100: '#e2ecfd',
+        200: '#7f1d1d',
+        DEFAULT: '#1d4ed8',
+      },
+    },
+  };
+
+
+
+
+
+
+
 }
--- apps/portfolio/tailwind.config.js
+++ apps/portfolio/tailwind.config.js
@@ -1,6 +1,48 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const path = require('path');
 const { join } = require('path');
+const colors = require('tailwindcss/colors');
+const angularMaterialPalettePlugin = require(path.resolve(__dirname, 'src/styles/theming.js'));
 
+
+const themes = {
+  // Default theme is required for theming system to work correctly!
+  'default': {
+    primary: {
+      ...colors.indigo,
+      DEFAULT: colors.indigo[600]
+    },
+    accent: {
+      ...colors.slate,
+      DEFAULT: colors.slate[800]
+    },
+    warn: {
+      ...colors.red,
+      DEFAULT: colors.red[600]
+    },
+    'on-warn': {
+      500: colors.red['50']
+    }
+  },
+  'teal': {
+    primary: {
+      ...colors.teal,
+      DEFAULT: colors.teal[600]
+    }
+  },
+  'rose': {
+    primary: colors.rose
+  },
+  'purple': {
+    primary: {
+      ...colors.purple,
+      DEFAULT: colors.purple[600]
+    }
+  },
+  'amber': {
+    primary: colors.amber
+  }
+};
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,7 +50,9 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+
+    extend: {
+    },
   },
-  plugins: [],
+  plugins: [require(path.resolve(__dirname, ('./src/styles/theming')))({ themes }),]
 };
