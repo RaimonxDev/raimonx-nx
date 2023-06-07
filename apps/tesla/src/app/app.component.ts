13cc1cc3abccd615838f94a7e1f463c3662843cb
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2023-06-07 19:46:48 -0400

    ng content for title section

--- apps/tesla/src/app/app.component.ts
+++ apps/tesla/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderTeslaComponent, HeroTeslaComponent, SectionTeslaComponent } from './components';
+import { HeaderTeslaComponent, SectionTeslaComponent } from './components';
 
 
 @Component({
   standalone: true,
-  imports: [HeroTeslaComponent, SectionTeslaComponent, HeaderTeslaComponent, RouterModule],
+  imports: [SectionTeslaComponent, HeaderTeslaComponent, RouterModule],
   selector: 'raimonx-dev-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- apps/tesla/src/app/components/index.ts
+++ apps/tesla/src/app/components/index.ts
@@ -1,3 +1,2 @@
 export * from './header-tesla/header-tesla.component';
-export * from './hero-tesla/hero-tesla.component';
 export * from './section-tesla/section-tesla.component';
