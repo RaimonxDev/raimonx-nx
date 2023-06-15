048ced467bb8f4125b8a2b4fe41d3b111d085f13
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2023-06-15 12:02:41 -0400

    complete chips

--- apps/mac-mini/src/app/app.component.ts
+++ apps/mac-mini/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NavGlobalComponent } from './components/nav/nav-global/nav-global.component';
 import { NavLocalComponent } from './components/nav/nav-local/nav-local.component';
 import { HeroComponent } from './components/sections/hero/hero.component';
+import { ChipComponent } from './components/sections/chip/chip.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, NavGlobalComponent, NavLocalComponent, HeroComponent],
+  imports: [CommonModule, RouterModule, NavGlobalComponent, NavLocalComponent, HeroComponent, ChipComponent],
   selector: 'mac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
