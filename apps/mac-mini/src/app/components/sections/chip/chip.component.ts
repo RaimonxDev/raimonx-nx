c6208c02ab62405eeffe2fe8e57ef45e5cec1f05
Ramon <ramonmartinezcordero@gmail.com>
2023-06-20 12:12:18 -0400

    add animation

--- apps/mac-mini/src/app/components/sections/chip/chip.component.ts
+++ apps/mac-mini/src/app/components/sections/chip/chip.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChildObserverDirective, RootObserverDirective } from '@raimonx-dev/directives';
 
 @Component({
   selector: 'mac-chip',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChildObserverDirective, RootObserverDirective],
   templateUrl: './chip.component.html',
   styleUrls: ['./chip.component.css'],
 })
-export class ChipComponent {}
+export class ChipComponent {
+
+  @ViewChildren('bar') bars!: QueryList<ElementRef>;
+
+  addAnimation($event: boolean) {
+    if (!$event) return;
+    this.bars.forEach((bar) => {
+      bar.nativeElement.classList.add(`animation-bar`);
+    });
+  }
+}
