2f3d615bc0b0c964fb3f5fd876649256188ca24b
Ramon <ramonmartinezcordero@gmail.com>
2023-06-23 00:42:04 -0400

    correciones

--- apps/portfolio/src/app/components/profile-overview/profile-overview.component.ts
+++ apps/portfolio/src/app/components/profile-overview/profile-overview.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RightSidebarComponent } from '../right-sidebar/right-sidebar.component';
 
 @Component({
   selector: 'raimonx-dev-profile-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RightSidebarComponent],
   templateUrl: './profile-overview.component.html',
   styleUrls: ['./profile-overview.component.css'],
 })
-export class ProfileOverviewComponent {}
+export class ProfileOverviewComponent { }
--- apps/portfolio/src/app/pages/projects/projects.component.ts
+++ apps/portfolio/src/app/pages/projects/projects.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'raimonx-dev-projects',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatButtonModule, MatTabsModule],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
 })
