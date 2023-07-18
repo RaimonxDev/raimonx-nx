f561be2ba6016bfb4d1d95ceb59b9ed175780a23
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2023-07-18 19:52:13 -0400

    card component

---
+++ apps/portfolio/src/app/shared/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+
+
+}
