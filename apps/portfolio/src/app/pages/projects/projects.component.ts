ece8e891eb12640393262fedc8f445abe706f36b
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2023-07-18 19:52:23 -0400

    update projects

--- apps/portfolio/src/app/pages/projects/projects.component.ts
+++ apps/portfolio/src/app/pages/projects/projects.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'raimonx-dev-projects',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatButtonModule, MatTabsModule],
+  imports: [CommonModule, RouterLink, MatButtonModule, MatTabsModule, CardComponent],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
 })
